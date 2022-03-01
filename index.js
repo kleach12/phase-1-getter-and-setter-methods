1ff5ecf973774505f6708d1369e928af0e4dd9fc
Kale Leach <kleach1212@gmail.com>
2022-02-28 18:00:12 -0800

    commit tree 88c40ec4d89632ee29ff0029334a405f8f74a12d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your Circle class here
+class Circle{
+  //#diameter;
+  //#circumfrence;
+  //#area;
+    
+  constructor(radius){
+    this.radius = radius;
+  }
+
+  get diameter(){
+    return this.radius * 2
+  }
+
+  get circumference(){
+    return this.diameter * Math.PI;
+  }
+
+  get area(){
+    return (this.radius * this.radius) * Math.PI;
+  }
+
+ set diameter(diameter) {
+    return this.radius  = diameter / 2
+  }
+
+  set circumference(circumference){
+    return this.radius  = (circumference/ Math.PI)/2;
+  }
+
+  set area(area){
+    return this.radius = Math.sqrt(area/ Math.PI);
+  }
+}
